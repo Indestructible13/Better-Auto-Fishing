56286e0ce4e76115ba82891e638e0e65d6b8bb4e
kinginde <kinginde@proton.me>
2026-02-08 23:50:45 -0500

    Added some files to git that I forgot to add before

---
+++ src/client/java/com/indestructible13/better_auto_fishing/BetterAutoFishingClient.java
@@ -0,0 +1,162 @@
+package com.indestructible13.better_auto_fishing;
+
+import com.indestructible13.better_auto_fishing.config.ConfigManager;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import com.indestructible13.better_auto_fishing.mixin.client.FishingBobberEntityAccessor;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
+
+public class BetterAutoFishingClient implements ClientModInitializer {
+    public static final String MOD_ID = "better_auto_fishing";
+
+    private enum AutoFishState {
+        IDLE,
+        REELING,
+        WAITING_FOR_CLEAR,
+        CASTING
+    }
+
+    private AutoFishState currentState = AutoFishState.IDLE;
+    private int tickCounter = 0;
+    private KeyBinding toggleActiveKey;
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Better Auto Fishing mod initializing...");
+
+        ClientTickEvents.END_CLIENT_TICK.register(this::onTick);
+
+        // Custom key binding
+        KeyBinding.Category CATEGORY = new KeyBinding.Category(
+                Identifier.of(MOD_ID, "custom_category")
+        );
+
+        toggleActiveKey = KeyBindingHelper.registerKeyBinding(
+                new KeyBinding(
+                        "key.better_auto_fishing.toggle_active", // The translation key for the key mapping.
+                        InputUtil.Type.KEYSYM, // The type of the keybinding; KEYSYM for keyboard, MOUSE for mouse.
+                        GLFW.GLFW_KEY_B, // The GLFW keycode of the key.
+                        CATEGORY // The category of the mapping.
+                ));
+    }
+
+    private void onTick(MinecraftClient client) {
+        PlayerEntity player = client.player; // Get player entity for later use
+        if (player == null) return;
+
+        // Toggle the mod active state when the toggle keybind is pressed
+        while (toggleActiveKey.wasPressed()) {
+            ConfigManager.setActive(!ConfigManager.getActive());
+            Text activatedMessage = Text.literal("Better Auto Fishing ").append(Text.literal("activated").formatted(Formatting.GREEN));
+            Text deactivatedMessage = Text.literal("Better Auto Fishing ").append(Text.literal("deactivated").formatted(Formatting.RED));
+            Utils.sendActionBarMessage(client, ConfigManager.getActive() ? activatedMessage : deactivatedMessage);
+        }
+
+        if (!ConfigManager.getActive()) { // If the mod is inactive, do nothing
+            resetState();
+            return;
+        }
+
+        FishingBobberEntity bobber = player.fishHook;
+
+        switch (currentState) {
+            case IDLE:
+                // While the bobber is null in the IDLE state, do nothing
+                // If the bobber appears while in IDLE, it means the player cast the rod
+                if (bobber != null) {
+                    // Use the Accessor Mixin to check the private caughtFish boolean
+                    boolean caughtFish = ((FishingBobberEntityAccessor) bobber).getCaughtFish();
+                    if (caughtFish) {
+                        currentState = AutoFishState.REELING;
+                        tickCounter = 0;
+                        //Utils.sendDebugChatMessage(player, "Caught fish");
+                        //Utils.sendDebugChatMessage(player, "Current state: " + currentState);
+                    }
+                }
+                break;
+
+            case REELING:
+                // If bobber is lost (e.g. manual reel in by the player), reset
+                if (bobber == null) {
+                    resetState();
+                    //Utils.sendDebugChatMessage(player, "Player manually reeled in");
+                    //Utils.sendDebugChatMessage(player, "Current state: " + currentState);
+                    return;
+                }
+
+                tickCounter++; // Count up to the reel delay, then reel in
+                Utils.sendDebugChatMessage(player, "Reel counter: " + tickCounter);
+                if (tickCounter >= ConfigManager.getReelDelay()) {
+                    reelIn(player, client);
+                    //Utils.sendDebugChatMessage(player, "Auto reeled in");
+                    currentState = AutoFishState.WAITING_FOR_CLEAR;
+                    //Utils.sendDebugChatMessage(player, "Current state: " + currentState);
+                    tickCounter = 0;
+                }
+                break;
+
+            case WAITING_FOR_CLEAR:
+                // Wait for the old bobber entity to be removed
+                // Prevents issues where the casting state would get reset because
+                // the old bobber was still in the water, but the mod thought
+                // the player had manually cast
+                if (bobber == null) {
+                    currentState = AutoFishState.CASTING;
+                    tickCounter = 0;
+                    //Utils.sendDebugChatMessage(player, "Bobber cleared");
+                    //Utils.sendDebugChatMessage(player, "Current state: " + currentState);
+                }
+                break;
+
+            case CASTING:
+                // If bobber appears (manual cast by player), reset
+                if (bobber != null) {
+                    resetState();
+                    //Utils.sendDebugChatMessage(player, "Player manually cast");
+                    //Utils.sendDebugChatMessage(player, "Current state: " + currentState);
+                    return;
+                }
+
+                tickCounter++; // Count up to the cast delay, then cast
+                Utils.sendDebugChatMessage(player, "Cast counter: " + tickCounter);
+                if (tickCounter >= ConfigManager.getCastDelay()) {
+                    castRod(player, client);
+                    //Utils.sendDebugChatMessage(player, "Auto cast");
+                    resetState();
+                    //Utils.sendDebugChatMessage(player, "Current state: " + currentState);
+                }
+                break;
+        }
+    }
+
+    private void resetState() {
+        currentState = AutoFishState.IDLE;
+        tickCounter = 0;
+    }
+
+    private static void reelIn(PlayerEntity player, MinecraftClient client) {
+        if (player != null && client.interactionManager != null) {
+            player.swingHand(Hand.MAIN_HAND);
+            client.interactionManager.interactItem(player, Hand.MAIN_HAND);
+            player.playSound(SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, 1.0F, 1.0F);
+        }
+    }
+
+    private static void castRod(PlayerEntity player, MinecraftClient client) {
+        if (player != null && client.interactionManager != null) {
+            player.swingHand(Hand.MAIN_HAND);
+            client.interactionManager.interactItem(player, Hand.MAIN_HAND);
+        }
+    }
+}
---
+++ src/client/java/com/indestructible13/better_auto_fishing/mixin/client/FishingBobberEntityAccessor.java
@@ -0,0 +1,11 @@
+package com.indestructible13.better_auto_fishing.mixin.client;
+
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(FishingBobberEntity.class)
+public interface FishingBobberEntityAccessor {
+    @Accessor("caughtFish")
+    boolean getCaughtFish();
+}
